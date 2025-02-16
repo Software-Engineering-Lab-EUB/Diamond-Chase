1bba1c936702a11d939ff506e2336e439603c8d5
unknown <n.t.sithy@gmail.com>
2025-02-16 10:48:15 +0600

    printing from a function from a branch

--- lab1.cpp
+++ lab1.cpp
@@ -1,7 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void print(){
+    cout<<"Printing from new function within new branch"<<endl;
+}
+
 int main(){
 
     cout<<"Lab1"<<endl;
+
+    print();
 }
